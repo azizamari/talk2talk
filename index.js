cebdad3fcd12a98c332b943f64ca27755a2f10b7
azizamari <azizamari2003@gmail.com>
2021-06-24 16:12:42 +0100

    socket.io connection working on client and server side

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const http =require('http');
 const path =require('path');
 const express =require('express');
+const socketio=require('socket.io');
 
-const server = http.createServer();
-const app = express(server);
+const app = express();
+const server = http.createServer(app);
+const io=socketio(server);
 
 app.use(express.static(path.join(__dirname,'public')));
+io.on('connection',socket=>{
+    console.log('New connection');
+});
 
 const PORT=process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
 });
