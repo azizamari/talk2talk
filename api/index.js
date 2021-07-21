3ebe08f12d88443c95c7dfbc9a986c72a43859af
azizamari <azizamari2003@gmail.com>
2021-07-21 10:52:13 +0100

    use room router in the api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const formatMessage=require('./utils/messages');
-const app = require('express')();
+const express = require('express');
+const app=express()
 const server = require('http').createServer(app);
 const sequelize = require('./utils/database');
 const room = require('./models/room');
 const message = require('./models/message');
+
+const roomRouter=require('./routers/room');
+
 const cors=require('cors');
 const io = require('socket.io')(server,{
     cors: {
@@ -13,6 +17,11 @@ const io = require('socket.io')(server,{
 });
 
 app.use(cors());
+app.use(express.json());
+
+
+app.use('/room',roomRouter);
+
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
